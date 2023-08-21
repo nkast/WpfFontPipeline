daf3a21be963f6daebfa59d6523e90f040fdeeb3
Nick Kastellanos <nkastellanos@gmail.com>
2023-08-21 15:12:25 +0300

    update to Kni v3.11.9002

--- Samples/Sample.DesktopGL/Game1.cs
+++ Samples/Sample.DesktopGL/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace DrawString.Common
+namespace DrawString
 {
 	/// <summary>
 	/// This is the main type for your game.
---
+++ Samples/Sample.DesktopGL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace DrawString
+{
+    public static class Program
+    { 
+        static void Main()
+        {
+            using (Game game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
